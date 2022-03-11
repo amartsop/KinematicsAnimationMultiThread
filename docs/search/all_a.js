35c32b8fe9ef74b555d96a302fbcbac4cbd5bedc
amartsop <amartsop@gmail.com>
2022-03-11 14:41:56 +0000

    docs_update

--- docs/search/all_a.js
+++ docs/search/all_a.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['kinematic_20three_2dfinger_20exoskeleton_20animation_0',['Kinematic Three-finger Exoskeleton Animation',['../md_README.html',1,'']]],
+  ['kinematic_20three_2dfinger_20exoskeleton_20animation_0',['Kinematic Three-finger Exoskeleton Animation',['../index.html',1,'']]],
   ['kinematic_5fanimation_2ecpp_1',['kinematic_animation.cpp',['../kinematic__animation_8cpp.html',1,'']]],
   ['kinematic_5fanimation_2eh_2',['kinematic_animation.h',['../kinematic__animation_8h.html',1,'']]],
   ['kinematicanimation_3',['KinematicAnimation',['../classKinematicAnimation.html',1,'KinematicAnimation'],['../classKinematicAnimation.html#a632c38a2fd57926db38179dd9d4ccacd',1,'KinematicAnimation::KinematicAnimation()']]]
--- docs/search/pages_0.js
+++ docs/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['kinematic_20three_2dfinger_20exoskeleton_20animation_0',['Kinematic Three-finger Exoskeleton Animation',['../md_README.html',1,'']]]
+  ['kinematic_20three_2dfinger_20exoskeleton_20animation_0',['Kinematic Three-finger Exoskeleton Animation',['../index.html',1,'']]]
 ];
